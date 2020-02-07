49c582dac3ad69e705aa2405c5daec10163cb79e
samuelLacaille <samuel76560@outlook.fr>
2020-02-07 16:35:29 +0100

    integration of the 2 modals in the header

--- Inc/header.php
+++ Inc/header.php
@@ -10,23 +10,27 @@
 
     <title>PICKME</title>
 
-    <link rel="stylesheet" href="assets/css/style.css">
+
     <link rel="stylesheet" href="assets/flexslider/flexslider.css" type="text/css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+
 
 </head>
 
 <body>
 
 <header>
-
     <div class="wrap_header">
         <div class="nav_container" id="nav_container">
             <div class="logo">
                 <img src="assets/img/logo.png" alt="logo de l'entreprise">
             </div>
 
-            <nav class="nav" id="nav"">
+            <nav class="nav" id="nav">
                 <ul>
                     <li><a href="../home.php">Home</a></li>
                     <li><a href="">Services</a></li>
@@ -35,11 +39,70 @@
                     <li><a href="">Link</a></li>
                 </ul>
             </nav>
+            <div id="dialog-form" title="Inscription">
+                <p class="validateTips">Les champs doivent tous être remplis..</p>
+
+                <form>
+                    <label class="modalLabel" for="nom">Nom :</label>
+                    <input type="text" name="nom" id="nom" value="" placeholder="Nom..."
+                           class="text ui-widget-content ui-corner-all"/>
+                    <label class="modalLabel" for="prenom">Prenom :</label>
+                    <input type="text" name="prenom" id="prenom" value="" placeholder="Prenom..."
+                           class="text ui-widget-content ui-corner-all"/>
+                    <label class="modalLabel" for="email">Email :</label>
+                    <input type="text" name="email" id="email" value="" placeholder="Email..."
+                           class="text ui-widget-content ui-corner-all"/>
+
+                    <label class="radioLine" for="recruteur">Recruteur :</label>
+                    <input type="radio" id="recruteur" name="role" value="recruteur"/>
+
+
+                    <label class="radioLine" for="candidat">Candidat :</label>
+                    <input type="radio" id="candidat" name="role" value="candidat"/>
+
+
+
+                    <label class="modalLabel" for="mdp">Mot de passe :</label>
+                    <input type="password" name="mdp" id="mdp" value="" placeholder="Mot de passe..."
+                           class="text ui-widget-content ui-corner-all"/>
+
+                    <!-- Allow form submission with keyboard without duplicating the dialog button -->
+                    <input type="submit" tabindex="-1" style="position:absolute; top:-1000px;">
+
+                </form>
+            </div>
+
+            <div id="dialog-form" title="Connexion">
+                <p class="validateTips">Les champs doivent tous être remplis..</p>
+
+                <form>
+
+                    <label class="modalLabel" for="email">Email :</label>
+                    <input type="text" name="email" id="email" value="" placeholder="Email..."
+                           class="text ui-widget-content ui-corner-all"/>
+
+                    <label class="radioLine" for="recruteur">Recruteur :</label>
+                    <input type="radio" id="recruteur" name="role" value="recruteur"/>
+
+
+                    <label class="radioLine" for="candidat">Candidat :</label>
+                    <input type="radio" id="candidat" name="role" value="candidat"/>
+
+
+                    <label class="modalLabel" for="mdp">Mot de passe :</label>
+                    <input type="password" name="mdp" id="mdp" value="" placeholder="Mot de passe..."
+                           class="text ui-widget-content ui-corner-all"/>
+
+                    <!-- Allow form submission with keyboard without duplicating the dialog button -->
+                    <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+
+                </form>
+            </div>
 
             <div class="connect">
                 <ul>
-                    <li><a href="">inscription</a></li>
-                    <li><a href="">connexion</a></li>
+                    <li id="create-user"><a>Inscription</a></li>
+                    <li id="connect-user"><a>Connexion</a></li>
                 </ul>
             </div>
 

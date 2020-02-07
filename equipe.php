9bacef6af0767994f36c2f9219bbfd8c4e8ce9b6
melanie deneuve blondel <melie.deneuve@outlook.com>
2020-02-07 12:20:16 +0100

    team page beginnings

---
+++ equipe.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+include('inc/header.php'); ?>
+
+<div class="wrappy">
+    <div class="equipe">
+
+        <div class="txtequipe">
+            <h1>L'équipe de Pick Me</h1>
+            <p></p>
+        </div>
+
+        <div class="facesequipe">
+            <img src="assets/img/avatarxandre.png" alt="Alexandre">
+            <img src="assets/img/avatarpierre.png" alt="Pierre">
+            <img src="assets/img/avatarmoi.png" alt="Melanie">
+            <img src="assets/img/avatarsamuel.png" alt="Samuel">
+            <img src="assets/img/avatarvictor.png" alt="Victor">
+        </div>
+
+    </div>
+</div>

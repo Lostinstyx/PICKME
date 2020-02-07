8bcfd5d53b913b6f5b59aa24015f8609df690a07
melanie deneuve blondel <melie.deneuve@outlook.com>
2020-02-07 14:41:47 +0100

    team page again

--- equipe.php
+++ equipe.php
@@ -7,17 +7,39 @@ include('inc/header.php'); ?>
     <div class="equipe">
 
         <div class="txtequipe">
-            <h1>L'équipe de Pick Me</h1>
-            <p></p>
+            <h1>L'équipe de Pick me</h1>
+            <p>Chez Pick me, toute notre équipe est mobilisée pour faciliter votre route vers le succès!</p>
         </div>
 
         <div class="facesequipe">
-            <img src="assets/img/avatarxandre.png" alt="Alexandre">
-            <img src="assets/img/avatarpierre.png" alt="Pierre">
-            <img src="assets/img/avatarmoi.png" alt="Melanie">
-            <img src="assets/img/avatarsamuel.png" alt="Samuel">
-            <img src="assets/img/avatarvictor.png" alt="Victor">
+            <div class="faces">
+                <img src="assets/img/avatarxandre.png" alt="Alexandre">
+                <h3>Alexandre</h3>
+                <p>Développeur Web Chef</p>
+            </div>
+            <div class="faces">
+                <img src="assets/img/avatarpierre.png" alt="Pierre">
+                <h3>Pierre</h3>
+                <p>Développeur Web Chef Adjoint</p>
+            </div>
+            <div class="faces">
+                <img src="assets/img/avatarmoi.png" alt="Melanie">
+                <h3>Mélanie</h3>
+                <p>Développeuse Web</p>
+            </div>
+            <div class="faces">
+                <img src="assets/img/avatarsamuel.png" alt="Samuel">
+                <h3>Samuel</h3>
+                <p>Développeur Web</p>
+            </div>
+            <div class="faces">
+                <img src="assets/img/avatarvictor.png" alt="Victor">
+                <h3>Victor</h3>
+                <p>Développeur Web</p>
+            </div>
         </div>
 
     </div>
 </div>
+
+<?php include('inc/footer.php'); ?>

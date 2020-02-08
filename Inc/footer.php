d5b52b798e2e6c90e81c0d7dca5ae18aa8793df8
Lostinstyx <courbois.alexandre76440@gmail.com>
2020-02-09 00:58:24 +0100

    switching jquerry version to being able to work with our login and connexion modals

--- Inc/footer.php
+++ Inc/footer.php
@@ -74,7 +74,7 @@
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="assets/flexslider/jquery.flexslider.js"></script>
 <script src="assets/js/main.js"></script>

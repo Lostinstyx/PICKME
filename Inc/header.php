8ff0ac468cfd4068be024cc6e254b271576ef737
Victor LEROUX <contact@fredericnoel.com>
2020-02-09 00:23:13 +0100

    Adding sections

---
+++ Inc/header.php
@@ -0,0 +1,10 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <title>PICKME</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/flexslider/flexslider.css" type="text/css">
+</head>
+<body>
+<header>

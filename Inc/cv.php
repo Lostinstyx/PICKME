a273ee03029942278f46bac8c1af82fbe052aebc
melanie deneuve blondel <melie.deneuve@outlook.com>
2020-02-26 11:38:25 +0100

    new branch chtemeleu +html page cv

---
+++ Inc/cv.php
@@ -0,0 +1,34 @@
+<div class="cv">
+    <h1>Votre CV</h1>
+    <div class="cvselect">
+        <select class=""></select>
+        <select class=""></select>
+        <select class=""></select>
+    </div>
+
+    <div class="cvmetierformation">
+        <div class="cvmetier">
+            <input type="text">
+        </div>
+
+        <div class="cvformation"
+            <input type="text">
+            <textarea></textarea>
+            <input type="text">
+            <textarea></textarea>
+        </div>
+
+        <div class="cvexperience"
+            <input type="text">
+            <textarea></textarea>
+            <input type="text">
+            <textarea></textarea>
+        </div>
+
+        <div class="cvcomplement">
+            <textarea></textarea>
+        </div>
+
+    </div>
+
+</div>

68f7c5571bd72d4778fca13048d71d17b4a13b0e
Pierre <pierre.halavent@gmail.com>
2020-02-27 12:15:01 +0100

    Register - add register form with class Form

--- inscription.php
+++ inscription.php
@@ -1,36 +1,55 @@
 <?php
-include('inc/header.php');
-?>
+spl_autoload_register();
 
-<div id="dialog-form" title="Inscription">
-    <p class="validateTips">Les champs doivent tous être remplis..</p>
-
-    <form>
-        <label for="nom">Nom :</label>
-        <input type="text" name="nom" id="nom" value="" placeholder="Nom..."
-               class="text ui-widget-content ui-corner-all"/>
-        <label for="prenom">Prenom :</label>
-        <input type="text" name="prenom" id="prenom" value="" placeholder="Prenom..."
-               class="text ui-widget-content ui-corner-all"/>
-        <label for="email">Email :</label>
-        <input type="text" name="email" id="email" value="" placeholder="Email..."
-               class="text ui-widget-content ui-corner-all"/>
-
-        <label class="radioLine" for="recruteur">Recruteur :</label>
-        <input type="radio" id="recruteur" name="role" value="recruteur"/>
+use \Inc\Service\Form;
+use \Inc\Service\Validation;
+use \Inc\Repository\ArticleRepository;
 
 
-        <label class="radioLine" for="candidat">Candidat :</label>
-        <input type="radio" id="candidat" name="role" value="candidat"/>
+$errors = array();
+
+$form = new Form($errors);
+
+if(!empty ($_POST['submitted'])) {
+    //Faille XSS
+    $title = trim(strip_tags($_POST['title']));
+    $content = trim(strip_tags($_POST['content']));
+
+    //validation
+    $v = new Validation();
+    $errors = $v->validChamp($errors, $title, 'titre', 3, 120);
+    $errors = $v->validChamp($errors, $title, 'titre', 3, 500);
+
+    if(count($errors) == 0) {
+        //insert into
+        $repo = new \Inc\Repository\ArticleRepository();
+        $newid = $repo->insert($title, $content);
 
 
+    }
 
-        <label for="mdp">Mot de passe :</label>
-        <input type="password" name="mdp" id="mdp" value="" placeholder="Mot de passe..."
-               class="text ui-widget-content ui-corner-all"/>
+}
 
-        <!-- Allow form submission with keyboard without duplicating the dialog button -->
-        <input type="submit" tabindex="-1" style="position:absolute; top:-1000px;">
 
+require_once ('Inc/header.php');?>
+
+
+    <form action="" method="post" style="margin-top: 100px">
+        <?= $form->label('choicecandidat', 'Candidat'); ?>
+        <?= $form->input('typeregister', 'radio') ?>
+        <?= $form->label('choicerecruteur', 'Recruteur'); ?>
+        <?= $form->input('typeregister', 'radio') ?>
+        <?= $form->label('prenom', 'Prenom'); ?>
+        <?= $form->input('prenom','text'); ?>
+        <?= $form->label('nom', 'Prenom'); ?>
+        <?= $form->input('nom','text'); ?>
+
+        <?= $form->error('title'); ?>
+
+        <div id="show"></div>
+        <?= $form->submit(); ?>
     </form>
-</div>
+
+
+
+<?php require_once ('Inc/footer.php');

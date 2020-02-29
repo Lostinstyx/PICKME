5ea14dda0941fffe19f83c5185f880823d150024
pierrehlvt <pierre.halavent@gmail.com>
2020-02-29 19:11:59 +0100

    Register - add page to choice the register form with a toggle button

---
+++ choice_inscription.php
@@ -0,0 +1,12 @@
+<?php require 'Inc/header.php' ?>
+<label class="button-toggle-wrap">
+    <p class="text-form"><a href = "#">Cliquez ici pour vous inscrire en tant que recruteur</a></p>
+    <input class="toggler" type="checkbox" data-toggle="button-toggle"/>
+    <div class="button-toggle">
+        <div class="handle">
+            <div class="bars"></div>
+        </div>
+    </div>
+</label>
+<div class="spacer"></div>
+<?php require 'Inc/footer.php' ?>

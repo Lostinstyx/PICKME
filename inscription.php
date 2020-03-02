a8d8d662f271e7f14b56eddc79d170b78fbe9a9d
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-03-02 16:45:15 +0100

    Work Of Pierre on inscription.php

--- inscription.php
+++ inscription.php
@@ -1,36 +1,137 @@
 <?php
-include('inc/header.php');
-?>
+spl_autoload_register();
 
-<div id="dialog-form" title="Inscription">
-    <p class="validateTips">Les champs doivent tous être remplis..</p>
-
-    <form>
-        <label for="nom">Nom :</label>
-        <input type="text" name="nom" id="nom" value="" placeholder="Nom..."
-               class="text ui-widget-content ui-corner-all"/>
-        <label for="prenom">Prenom :</label>
-        <input type="text" name="prenom" id="prenom" value="" placeholder="Prenom..."
-               class="text ui-widget-content ui-corner-all"/>
-        <label for="email">Email :</label>
-        <input type="text" name="email" id="email" value="" placeholder="Email..."
-               class="text ui-widget-content ui-corner-all"/>
-
-        <label class="radioLine" for="recruteur">Recruteur :</label>
-        <input type="radio" id="recruteur" name="role" value="recruteur"/>
+use \Inc\Service\Form;
+use \Inc\Service\Validation;
+use \Inc\Repository\ArticleRepository;
 
 
-        <label class="radioLine" for="candidat">Candidat :</label>
-        <input type="radio" id="candidat" name="role" value="candidat"/>
+$errors = array();
+
+$form = new Form($errors);
+
+if(!empty ($_POST['submitted'])) {
+    //Faille XSS
+    $prenom = trim(strip_tags($_POST['prenom']));
+    $nom = trim(strip_tags($_POST['nom']));
+    $email = trim(strip_tags($_POST['email']));
+    $telephone = trim(strip_tags($_POST['telephone']));
+    $entreprise = trim(strip_tags($_POST['entreprise']));
+    $siret = trim(strip_tags($_POST['siret']));
+    $street = trim(strip_tags($_POST['street']));
+    $postalcode = trim(strip_tags($_POST['postalcode']));
+    $city = trim(strip_tags($_POST['city']));
+    $password1 = trim(strip_tags($_POST['password1']));
+    $password2 = trim(strip_tags($_POST['password2']));
 
 
+    //validation
+    $v = new Validation();
+    $errors = $v->validChamp($errors, $nom, 'nom', 2, 50);
+    $errors = $v->validChamp($errors, $prenom, 'prenom', 2, 20);
+    $errors = $v->validChamp($errors, $telephone, 'telephone', 2, 20);
+    $errors = $v->validChamp($errors, $entreprise, 'entreprise', 2, 30);
+    $errors = $v->validChamp($errors, $siret, 'siret', 14, 14);
+    $errors = $v->validChamp($errors, $street, 'street', 5, 100);
+    $errors = $v->validChamp($errors, $postalcode, 'postalcode', 5, 5);
+    $errors = $v->validChamp($errors, $city, 'city', 2, 40);
 
-        <label for="mdp">Mot de passe :</label>
-        <input type="password" name="mdp" id="mdp" value="" placeholder="Mot de passe..."
-               class="text ui-widget-content ui-corner-all"/>
+    $errors = $v->validPassword($errors, $password1, $password2, 'password');
+    $errors = $v->validMail($errors, $email, 'email');
 
-        <!-- Allow form submission with keyboard without duplicating the dialog button -->
-        <input type="submit" tabindex="-1" style="position:absolute; top:-1000px;">
+    if(count($errors) == 0) {
+        //insert into
+        die('OK MA COUILLE');
+        //$repo = new \Inc\Repository\ArticleRepository();
+        //  $newid = $repo->insert($title, $content);
 
+
+    }
+
+}
+
+
+require_once ('Inc/header.php');?>
+
+
+    <form class="searchCV" action="" method="post" style="margin-top: 100px">
+        <?= $form->label('choicecandidat', 'Candidat'); ?>
+        <?= $form->radio('typeregister', 'choicecandidat') ?>
+        <?= $form->label('choicerecruteur', 'Recruteur'); ?>
+        <?= $form->radio('typeregister', 'choicerecruteur') ?>
+
+
+        <?= $form->error('title'); ?>
     </form>
-</div>
+
+    <form class="searchCV" id="form-recruteur" action="" method="post">
+        <?= $form->label('prenom', 'Prenom'); ?>
+        <?= $form->input('prenom','text'); ?>
+
+        <?= $form->label('nom', 'Nom'); ?>
+        <?= $form->input('nom','text'); ?>
+
+        <?= $form->label('email', 'Email'); ?>
+        <?= $form->input('email','email'); ?>
+
+        <?= $form->label('telephone', 'telephone'); ?>
+        <?= $form->input('telephone','text'); ?>
+
+        <?= $form->label('entreprise', 'Entreprise'); ?>
+        <?= $form->input('entreprise','text'); ?>
+
+        <?= $form->label('street', 'Adresse'); ?>
+        <?= $form->input('street','text'); ?>
+
+        <?= $form->label('postalcode', 'Code Postal'); ?>
+        <?= $form->input('postalcode','text'); ?>
+
+        <?= $form->label('city', 'Ville'); ?>
+        <?= $form->input('city','text'); ?>
+
+        <?= $form->label('siret', 'N° de Siret'); ?>
+        <?= $form->input('siret','text'); ?>
+
+        <?= $form->label('password1', 'Mot de passe'); ?>
+        <?= $form->input('password1','password'); ?>
+
+        <?= $form->label('password2', 'Valider votre mot de passe'); ?>
+        <?= $form->input('password2','password'); ?>
+
+        <?= $form->submit(); ?>
+    </form>
+
+    <form class="searchCV" id="form-candidat" action="" method="post">
+        <?= $form->label('prenom', 'Prenom'); ?>
+        <?= $form->input('prenom','text'); ?>
+
+        <?= $form->label('nom', 'Nom'); ?>
+        <?= $form->input('nom','text'); ?>
+
+        <?= $form->label('email', 'Email'); ?>
+        <?= $form->input('email','email'); ?>
+
+        <?= $form->label('telephone', 'telephone'); ?>
+        <?= $form->input('telephone','text'); ?>
+
+        <?= $form->label('street', 'Adresse'); ?>
+        <?= $form->input('street','text'); ?>
+
+        <?= $form->label('postalcode', 'Code Postal'); ?>
+        <?= $form->input('postalcode','text'); ?>
+
+        <?= $form->label('city', 'Ville'); ?>
+        <?= $form->input('city','text'); ?>
+
+        <?= $form->label('password1', 'Mot de passe'); ?>
+        <?= $form->input('password1','password'); ?>
+
+        <?= $form->label('password2', 'Valider votre mot de passe'); ?>
+        <?= $form->input('password2','password'); ?>
+
+        <?= $form->submit(); ?>
+    </form>
+
+
+
+<?php require_once ('Inc/footer.php');

9db99b8d3dc7c2d2026e06e6a08b44c1f0cb0442
melanie deneuve blondel <melie.deneuve@outlook.com>
2020-03-02 11:41:54 +0100

    beginnings of the cv profil page + creation branch profil

---
+++ profil.php
@@ -0,0 +1,56 @@
+<h1>Mon profil</h1>
+
+
+
+<?php
+
+include('inc/pdo.php');
+include('inc/function/debug.php');
+
+$title = 'cv';
+$sql = "SELECT * FROM cv";
+$query = $pdo->prepare($sql);
+$query->execute();
+$cv = $query->fetchAll();
+//debug($cv);
+
+foreach ($cv as $key => $donnees) {
+    echo $donnees;
+}
+
+?>
+
+<table>
+    <tr>
+        <td>id: <?php echo $donnees['id'];?></td>
+    </tr>
+    <tr>
+        <td>Catégorie: <?php echo $donnees['category'];?></td>
+    </tr>
+    <tr>
+        <td>Etudes: <?php echo $donnees['study'];?></td>
+    </tr>
+    <tr>
+        <td>Experience: <?php echo $donnees['experience'];?></td>
+    </tr>
+    <tr>
+        <td>Métier: <?php echo $donnees['work'];?></td>
+    </tr>
+    <tr>
+        <td>Intitulé Formation 1: <?php echo $donnees['title_formation1'];?></td>
+        <td>Formation 1: <?php echo $donnees['formation1'];?></td>
+        <td>Intitulé Formation 2: <?php echo $donnees['title_formation2'];?></td>
+        <td>Formation 2: <?php echo $donnees['formation2'];?></td>
+    </tr>
+    <tr>
+        <td>Intitulé Expérience 1: <?php echo $donnees['title_experience1'];?></td>
+        <td>Expérience 1: <?php echo $donnees['experience1'];?></td>
+        <td>Intitulé Expérience 2: <?php echo $donnees['title_experience2'];?></td>
+        <td>Expérience 2: <?php echo $donnees['experience2'];?></td>
+    </tr>
+    <tr>
+        <td>Informations complémentaires: <?php echo $donnees['informations'];?></td>
+    </tr>
+
+</table>
+

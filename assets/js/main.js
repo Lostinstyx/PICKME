dc0f8d2113c4e282dd624f9cbd6b4fc01d7c7f01
Pierre <pierre.halavent@gmail.com>
2020-03-04 12:31:45 +0100

    [WIP] CVBuilder
    [TODO] - Send form with only 1 input submit (ajax ?)

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,20 +10,20 @@ $(window).load(function () {
         var i=1;
         $('#add').click(function(){
             i++;
-            $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name[]" placeholder="Ajouter une formation" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Intitulé du diplôme" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Etablissement" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Tâches effectuées lors de votre formation" class="form-control name_list" /></td>' +
+            $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="add_formationn" placeholder="Ajouter une formation" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_diplome[]" placeholder="Intitulé du diplôme" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_lieu[]" placeholder="Etablissement" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_work[]" placeholder="Tâches effectuées lors de votre formation" class="form-control name_list" /></td>' +
                 '<td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
         });
         $('#add_xp').click(function(){
             i++;
-            $('#dynamic_field_xp').append('<tr id="row'+i+'"><td><input type="text" name="name[]" placeholder="Ajouter une expérience" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Intitulé du poste" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Entreprise" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Lieu" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Description des tâches effectués" class="form-control name_list" /></td>' +
-                '<td><input type="text" name="name[]" placeholder="Période" class="form-control name_list" /></td>' +
+            $('#dynamic_field_xp').append('<tr id="row'+i+'"><td><input type="text" name="add_xpe[]" placeholder="Ajouter une expérience" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_post[]" placeholder="Intitulé du poste" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_entreprise[]" placeholder="Entreprise" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_place[]" placeholder="Lieu" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_taches[]" placeholder="Description des tâches effectués" class="form-control name_list" /></td>' +
+                '<td><input type="text" name="add_time[]" placeholder="Période" class="form-control name_list" /></td>' +
                 '<td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
 
         });
@@ -33,18 +33,6 @@ $(window).load(function () {
             $('#row'+button_id+'').remove();
         });
 
-        $('#submit').click(function(){
-            $.ajax({
-                url:"name.php",
-                method:"POST",
-                data:$('#add_name').serialize(),
-                success:function(data)
-                {
-                    alert(data);
-                    $('#add_name')[0].reset();
-                }
-            });
-        });
 
  // Modal inscription
     var dialog, form,
